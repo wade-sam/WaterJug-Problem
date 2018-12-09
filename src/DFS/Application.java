8aec72cf21928686ecd0f4483b61050161d88ec2
wade-sam <Sammy@idnet.co.uk>
2018-12-09 22:17:31 +0000

    started implementing the waterjug logic

--- src/DFS/Application.java
+++ src/DFS/Application.java
@@ -2,10 +2,11 @@ package DFS;
 import java.util.Stack;
 import java.util.Set;
 import java.util.Scanner;
+import java.util.HashSet;
 public class Application {
 	
 	public static Stack<Node> stack = new Stack<>(); 
-	public  static Set<Node> loggedNodesExisting;
+	public  static Set<Node> StateExists;
 	public static int JugAcapacity;
 	public static int JugBcapacity;
 	public static int JugCcapacity;
@@ -27,7 +28,39 @@ public class Application {
 			Node root = new Node(0,0,0);
 			stack.push(root);
 			System.out.print(stack);
+				StateExists = new HashSet<>();
+				StateExists.add(root);
+				Stack<Integer>CurrentLevelTracker = new Stack<>();
+				CurrentLevelTracker.push(0);
+				int NoNodes = 0;
 				
-		
+				while(!stack.isEmpty()) {
+					NoNodes++;
+					int temp = 0;
+					Node node = stack.pop();
+					int currentLevel = CurrentLevelTracker.pop();
+					
+					if (node.A == JugAcapacity && node.B == 0 && JugAcapacity > Gu) {
+						if (node.C == 0) {
+							Node nextNode = new Node(JugAcapacity-JugBcapacity, JugBcapacity,0);
+						}
+						else {
+							Node nextNode = new Node(JugAcapacity-JugBcapacity, JugBcapacity,node.C);
+						}
+						
+						
+					} else if (node.A == JugAcapacity && node.C == 0) {
+						if (node.B == 0) {
+							Node nextNode = new Node (JugAcapacity-JugCcapacity, 0, JugCcapacity);
+						}
+						else {
+							Node nextNode = new Node (JugAcapacity-JugCcapacity, node.B, JugCcapacity);
+						}
+						
+						
+					}
+				}
+
 	}
+	
 }
