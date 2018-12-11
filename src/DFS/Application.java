905175d1ceece41372d77f299653fa1cd6777e42
wade-sam <Sammy@idnet.co.uk>
2018-12-11 23:44:36 +0000

    implemented recursion

--- src/DFS/Application.java
+++ src/DFS/Application.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class Application {
 	
 	public static Stack<Node> stack = new Stack<>(); 
-	public static ArrayList  nodeComparison = new ArrayList();
+	//public static ArrayList nodeComparison = new ArrayList();
 	public static Set<Node> StateExists;
 	public static int JugAcapacity;
 	public static int JugBcapacity;
@@ -17,7 +17,7 @@ public class Application {
 	public static int jugB = 0;
 	public static int jugC = 0;
 	public static int count = 0;
-	public static Stack<Integer>CurrentLevelTracker = new Stack<>();
+	
 	public static void main(String args[]) {
 		Scanner UserInput = new Scanner(System.in);
 		System.out.println("Please Input the Capacity for Jug A: ");
@@ -32,14 +32,16 @@ public class Application {
 		System.out.println(JugCcapacity);
 		
 		
-			int treeLevel = 0;
-			Node root = new Node(0,0,0);
+			Node root = new Node();
+			root.a = 0;
+			root.b = 0;
+			root.c = 0;
 			stack.push(root);
 			System.out.print(stack);
 				StateExists = new HashSet<>();
 				StateExists.add(root);
 				//nodeComparison.add(root)
-
+				Stack<Integer>CurrentLevelTracker = new Stack<>();
 				CurrentLevelTracker.push(0);
 				int NoNodes = 0;
 				boolean test = true;
@@ -49,240 +51,169 @@ public class Application {
 					NoNodes++;
 					int temp = 0;
 					Node node = stack.pop();
-					//int currentLevel = CurrentLevelTracker.pop();
-			if (jugA == 0 || jugB == 0 || jugC == 0) {
-				if (jugA == 0) {
-					jugA = JugAcapacity + jugA;
-					Node nextNode = new Node(jugA,jugB,jugC);
-					
-					if(alreadyExists(nextNode)) {
-						CurrentLevelTracker.push(currentLevel + 1);
-						System.out.print(" return1");
-					}
-					
-				}
-				if (jugB == 0) {		 
-					 jugB = JugBcapacity + jugB;
-					Node nextNode = new Node(jugA,JugBcapacity,jugC);
-					if(alreadyExists(nextNode)) {
-						CurrentLevelTracker.push(currentLevel + 1);
-						System.out.print(" return2");
-					}
-					
-				}
-				 if (jugC == 0) {
-					 jugC = JugCcapacity + jugC;
-					Node nextNode = new Node(jugA,jugB,JugCcapacity);
-					if(alreadyExists(nextNode)) {
-						CurrentLevelTracker.push(currentLevel + 1);
-						System.out.print(" return3");
-					}
-			}
-			}else if (jugA == JugAcapacity || jugB == JugBcapacity || jugC == JugCcapacity) {
+					int currentLevel = CurrentLevelTracker.pop();
 				
-				if (jugA == JugAcapacity) {
-				Node nextNode = new Node(0,jugB,jugC);
-				jugA = 0;
-					if (alreadyExists(nextNode)) {
-						CurrentLevelTracker.push(currentLevel + 1);
-						System.out.print(" return4");
-					}
-				}
-			
-			 if (jugB == JugBcapacity) {
-				 Node nextNode = new Node(jugA,0,jugC);
-				jugB=0;
-				if (alreadyExists(nextNode)) {
-					CurrentLevelTracker.push(currentLevel + 1);
-					System.out.print(" return5");
-				}
-				
-			}
-			 
-			 if (jugC == JugCcapacity) {
-				 Node nextNode = new Node(jugA, jugB, 0);
-				 jugC = 0;
-				 if (alreadyExists(nextNode)) {
-					 CurrentLevelTracker.push(currentLevel + 1);
-					 System.out.print("return 6");
-				 }
-			 }
-				
-				
-			}else if (jugA != 0 && jugB != JugBcapacity || jugA != 0 && jugC != JugCcapacity || jugB != 0 && jugA != JugAcapacity || jugB != 0 && jugC != JugCcapacity || jugC != 0 && jugA != JugAcapacity || jugC != 0 && jugB != JugBcapacity) {
-				if (jugA != 0 && jugB != JugBcapacity) {
-					 System.out.print(" REACHED 7 ");
-						int outcome[] = pourJugs(jugA, jugB, JugBcapacity);
-						jugA = outcome[0];
-						jugB = outcome[1];
-						Node nextNode = new Node (jugA, jugB, jugC);
-						if (alreadyExists(nextNode)) {
-						CurrentLevelTracker.push(currentLevel + 1);
-						System.out.print(" return7");
-					}
-					
-					}
-					 if (jugA != 0 && jugC != JugCcapacity) {
-						 System.out.print(" REACHED 8 ");
-						int outcome[] = pourJugs(jugA, jugC, JugCcapacity);
-						 jugA = outcome[0];
-						 jugC = outcome[1];
-						Node nextNode = new Node (jugA, jugB, jugC);
-						if (alreadyExists(nextNode)) {
-							CurrentLevelTracker.push(currentLevel + 1);
-							System.out.print(" return8");
-						}
-					
-					}
-					 if (jugB != 0 && jugA != JugAcapacity) {
-						 System.out.print(" REACHED 9 ");
-						int outcome[] = pourJugs(jugB, jugA, JugAcapacity);
-						 jugB = outcome[0];
-						 jugA = outcome[1];
-						Node nextNode = new Node (jugA, jugB, jugC);
-						if (alreadyExists(nextNode)) {
-							CurrentLevelTracker.push(currentLevel + 1);
-							System.out.print(" return9");
-						}
-						
-					}
-					 if (jugB != 0 && jugC != JugCcapacity) {
-						 System.out.print(" REACHED 10 ");
-						int outcome[] = pourJugs(jugB, jugC, JugCcapacity);
-						 jugB = outcome[0];
-						 jugC = outcome[1];
-						Node nextNode = new Node (jugA, jugB, jugC);
-						if (alreadyExists(nextNode)) {
-							CurrentLevelTracker.push(currentLevel + 1);
-							System.out.print(" return10");
-						}
-					
-					}
-					 if (jugC != 0 && jugA != JugAcapacity) {
-						 System.out.print(" REACHED 11 ");
-						int outcome[] = pourJugs(jugC, jugA, JugAcapacity);
-						jugC = outcome[0];
-						jugA = outcome[1];
-						System.out.print(jugC);
-						Node nextNode = new Node (jugA, jugB, jugC);
-						if (alreadyExists(nextNode)) {
-							CurrentLevelTracker.push(currentLevel + 1);
-							System.out.print(" return11");
-						}
-					
-					}
-					 if (jugC != 0 && jugB != JugBcapacity) {
-						 System.out.print(" REACHED 12 ");
-						int outcome[] = pourJugs(jugC, jugB, JugBcapacity);
-						jugC = outcome[0];
-						jugB = outcome[1];
-						Node nextNode = new Node (jugA, jugB, jugC);
-						if (alreadyExists(nextNode)) {
-							CurrentLevelTracker.push(currentLevel + 1);
-							System.out.print(" return12");
-						}
-					
 			//System.out.print(count);
 		  }
-			} 
-				
-				
-				
- 
-				
-				
-				
-				
+	
 			}
-						
-						
-					
-
-					
-						
-						
-						
-						 
-						 
-
-					}
-					/*
-					if (node.A == JugAcapacity && node.B == 0 && JugAcapacity > Gu) {
-						if (node.C == 0) {
-							Node nextNode = new Node(JugAcapacity-JugBcapacity, JugBcapacity,0);
-						}
-						else {
-							Node nextNode = new Node(JugAcapacity-JugBcapacity, JugBcapacity,node.C);
-						}
-						
-						
-					} else if (node.A == JugAcapacity && node.C == 0) {
-						if (node.B == 0) {
-							Node nextNode = new Node (JugAcapacity-JugCcapacity, 0, JugCcapacity);
-						}
-						else {
-							Node nextNode = new Node (JugAcapacity-JugCcapacity, node.B, JugCcapacity);
-						}
-						
-						
-					}
-					*/
-	public static void SendToAlreadyExists(Node node) {
-		if (alreadyExists(node)) {
-			CurrentLevelTracker.push(currentLevel + 1);
+	
+	private void logic (Node node) {
+		System.out.print(node.a +" " + node.b +" " + node.c + " ");
+		List<Node> neighbours=node.getNeighbours();
+		node.visited=true;
+		for (int i=0; i < neighbours.size(); i++) {
+			Node n=neighbours.get(i);
+			if(n!=null && n.visited) {
+				logic(n);
+			}
 		}
-		
 	}
-
+	
+	
+	private static Node fillUpA(Node aN) {
+		Node tempNode = new Node(aN.a,aN.b,aN.c);
+		aN.a = JugAcapacity;
+		tempNode.b = aN.a;
+		tempNode.c = aN.b;
+		return tempNode;
+	}
+	private static Node fillB(Node bN) {
+		Node tempNode = new Node(bN.a, bN.b,bN.c);
+		tempNode.a = bN.a;
+		tempNode.b = JugBcapacity;
+		tempNode.c = bN.c;
+		return tempNode;
+	}
+	private static Node fillC(Node cN) {
+		Node tempNode = new Node();
+		tempNode.a = cN.a;
+		tempNode.b = cN.b;
+		tempNode.c = JugCcapacity;
+		return tempNode;
+	}
+	private static Node EmptyA(Node aE) {
+		Node tempNode = new Node();
+		tempNode.a = 0;
+		tempNode.b = aE.b;
+		tempNode.c = aE.c;
+		return tempNode;
+	}
+	private static Node EmptyB(Node bE){
+		Node tempNode = new Node();
+		tempNode.a = bE.a;
+		tempNode.b = 0;
+		tempNode.c = bE.c;
+		return tempNode;
+	}
+	private static Node EmptyC(Node cE) {
+		Node tempNode = new Node();
+		tempNode.a = cE.a;
+		tempNode.b = cE.b;
+		tempNode.c = 0;
+		return tempNode;
+	}
+	
+	private static Node aTObTransfer(Node Ab) {
+		Node t = new Node();
+		t.a = Ab.a;
+		t.b = Ab.b;
+		t.c = Ab.c;
+		if((t.a + t.b) > JugBcapacity) {
+			t.a = (t.a + t.b) - JugBcapacity;
+			Math.abs(t.a);
+			
+		}else if ((t.a + t.b) <= JugBcapacity){
+			t.b = t.a + t.b;
+			t.a = 0;
+		}
+		return t;
+	}
+	
+	private static Node aTOcTransfer(Node Ac) {
+		Node t = new Node();
+		t.a = Ac.a;
+		t.b = Ac.b;
+		t.c = Ac.c;
+		
+		if((t.a + t.c)>JugCcapacity) {
+			t.a  = (t.a + t.c) - JugCcapacity;
+			Math.abs(t.a);
+		}else if((t.a + t.c)<=JugCcapacity) {
+			t.c = t.a + t.c;
+			t.a = 0;
+		}
+		return t;
+	}
+	private static Node bTOaTransfer(Node Ba) {
+		Node t = new Node();
+		t.a = Ba.a;
+		t.b = Ba.b;
+		t.c = Ba.c;
+		if((t.b + t.a)>JugAcapacity) {
+			t.b  = (t.b + t.a) - JugAcapacity;
+			Math.abs(t.b);
+		}else if((t.b + t.a)<=JugAcapacity) {
+			t.a = t.b + t.a;
+			t.b = 0;
+		}
+		return t;
+	}
+	private static Node bTOcTransfer(Node Bc) {
+		Node t = new Node();
+		t.a = Bc.a;
+		t.b = Bc.b;
+		t.c = Bc.c;
+		if((t.b + t.c)>JugCcapacity) {
+			t.b  = (t.b + t.c) - JugCcapacity;
+			Math.abs(t.b);
+		}else if((t.b + t.c)<=JugAcapacity) {
+			t.c = t.b + t.c;
+			t.b = 0;
+		}
+		return t;
+	}
+	private static Node cTOaTransfer(Node Ca) {
+		Node t = new Node();
+		t.a = Ca.a;
+		t.b = Ca.b;
+		t.c = Ca.c;
+		if((t.c + t.a)>JugAcapacity) {
+			t.c  = (t.c + t.a) - JugAcapacity;
+			Math.abs(t.c);
+		}else if((t.c + t.a)<=JugAcapacity) {
+			t.a = t.c + t.a;
+			t.c = 0;
+		}
+		return t;
+	}
+	private static Node cTObTransfer(Node Cb) {
+		Node t = new Node();
+		t.a = Cb.a;
+		t.b = Cb.b;
+		t.c = Cb.c;
+		if((t.c + t.b)>JugBcapacity) {
+			t.c  = (t.c + t.b) - JugBcapacity;
+			Math.abs(t.c);
+		}else if((t.c + t.b)<=JugBcapacity) {
+			t.b = t.c + t.b;
+			t.c = 0;
+		}
+		return t;
+	}
+	
 	public static boolean alreadyExists(Node node) {
 		if(!StateExists.contains(node)) {
 			stack.push(node);
 			StateExists.add(node);
-			System.out.print(" returnTRUE" + jugA + jugB + jugC);
 			count++;
+			System.out.print(" returnTRUE" + jugA + jugB + jugC + count);
+			
 			return true;
 		}
 			return false;
 			
 		}
-	/*
-	public static boolean checkIfExists(Node nodeC) {
-		StringBuilder temp = new StringBuilder();
-		temp.append(jugA);
-		temp.append(jugB);
-		temp.append(jugC);
-		
-		
-		//System.out.print(temp);
-		if(!nodeComparison.contains(nodeC)) {
-			stack.push(nodeC);
-			nodeComparison.add(nodeC);
-			//System.out.println(nodeComparison);
-			System.out.print("returnTRUE" + jugA + jugB + jugC);
-			return false;
-		}
-		System.out.print("returnFALSE");
-		return true;
-	}
-	*/
-	public static int[] pourJugs(int x, int y, int z) {
-		System.out.print("reached");
-		int changes[] = new int[3];
-		if((x + y) > z) {
-			x = (x + y) - z;
-			y = z;
-			Math.abs(x);
-			changes[0] = x;
-			changes[1] = y;
-			changes[2] = z;
-		}else if ((x + y) <= z){
-			y = x + y;
-			x = 0;
-			changes[0] = x;
-			changes[1] = y;
-			changes[2] = z;
-		}
-		return changes;
-	}
+
 	
 }
